620a5a066f2a03a0a8619ed2c98047749c28f5ac
Ali Dowlatshahi <alidowlatshahi@Alis-MacBook-Pro-2.local>
2023-12-23 14:45:17 -0600

    rtf editor

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,63 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the modal elements and the forms inside them
-  var modal = document.getElementById("editModal");
-  var editForm = document.getElementById("edit-post-form");
   var createModal = document.getElementById("createModal");
   var createPostBtn = document.getElementById("create-post-btn");
+  var editModal = document.getElementById("editModal");
+  var editForm = document.getElementById("edit-post-form");
+
+  // Function to initialize TinyMCE
+  function initializeTinyMCE() {
+    if (!tinymce.get("create-post-content")) {
+      console.log("Initializing TinyMCE");
+      tinymce.init({
+        selector: "#create-post-content",
+        // ... TinyMCE configuration ...
+      });
+    } else {
+      console.log("TinyMCE already initialized");
+    }
+  }
+
+  // Event listener for opening the create post modal
+  createPostBtn.onclick = function () {
+    console.log("Create Post button clicked");
+    createModal.style.display = "block";
+    initializeTinyMCE();
+  };
+
+  // Event listener for the create post form submission
+  document
+    .getElementById("create-post-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      const title = document.getElementById("create-post-title").value;
+      const content = tinymce.get("create-post-content").getContent();
+
+      fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, content }),
+      })
+        .then((response) => response.json())
+        .then(() => {
+          createModal.style.display = "none";
+          if (tinymce.get("create-post-content")) {
+            tinymce.get("create-post-content").remove();
+          }
+          fetchPosts();
+        })
+        .catch((error) => console.error("Error creating post:", error));
+    });
+
+  // Close modal logic
+  document.getElementsByClassName("create-modal-close")[0].onclick =
+    function () {
+      createModal.style.display = "none";
+      if (tinymce.get("create-post-content")) {
+        tinymce.get("create-post-content").remove();
+      }
+    };
 
   // Function to fetch and display posts
   function fetchPosts() {
@@ -12,7 +66,6 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((posts) => {
         const postsContainer = document.getElementById("blog-posts");
         postsContainer.innerHTML = "";
-
         posts.forEach((post) => {
           const postElement = document.createElement("div");
           postElement.className = "post-card";
@@ -38,21 +91,12 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("edit-post-title").value = post.title;
         document.getElementById("edit-post-content").value = post.content;
         editForm.dataset.id = id;
-        modal.style.display = "block";
+        editModal.style.display = "block";
       })
       .catch((error) => console.error("Error fetching post:", error));
   };
 
-  // Attach event listeners
-  createPostBtn.onclick = function () {
-    createModal.style.display = "block";
-  };
-
-  document.getElementsByClassName("create-modal-close")[0].onclick =
-    function () {
-      createModal.style.display = "none";
-    };
-
+  // Event listener for the edit post form submission
   editForm.onsubmit = function (e) {
     e.preventDefault();
     const id = editForm.dataset.id;
@@ -68,78 +112,11 @@ document.addEventListener("DOMContentLoaded", function () {
     })
       .then((response) => response.json())
       .then(() => {
-        modal.style.display = "none";
+        editModal.style.display = "none";
         fetchPosts();
       })
       .catch((error) => console.error("Error updating post:", error));
   };
-
-  document
-    .getElementById("create-post-form")
-    .addEventListener("submit", function (e) {
-      e.preventDefault();
-      const title = document.getElementById("create-post-title").value;
-      const content = document.getElementById("create-post-content").value;
-
-      fetch("/api/posts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ title, content }),
-      })
-        .then((response) => response.json())
-        .then(() => {
-          createModal.style.display = "none";
-          fetchPosts();
-        })
-        .catch((error) => console.error("Error creating post:", error));
-    });
-
-  document.getElementsByClassName("close")[0].onclick = function () {
-    modal.style.display = "none";
-  };
-
-  window.onclick = function (event) {
-    if (event.target == createModal) {
-      createModal.style.display = "none";
-    }
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-
-  // Function to delete a post
-  window.deletePost = function (id) {
-    if (!confirm("Are you sure you want to delete this post?")) return;
-
-    fetch(`/api/posts/${id}`, {
-      method: "DELETE",
-    })
-      .then(() => {
-        fetchPosts();
-      })
-      .catch((error) => console.error("Error deleting post:", error));
-  };
-
-  // Function to create a new post
-  function createPost() {
-    const title = document.getElementById("post-title").value;
-    const content = document.getElementById("post-content").value;
-
-    fetch("/api/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ title, content }),
-    })
-      .then((response) => response.json())
-      .then(() => {
-        fetchPosts();
-      })
-      .catch((error) => console.error("Error creating post:", error));
-  }
 
   // Initially fetch and display all posts
   fetchPosts();
